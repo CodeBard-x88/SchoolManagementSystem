f8903f8ee167afe5ef8aabef19e3c2cf10c523c1
Muhammad Tayyab <solo.developer29@gmail.com>
2023-12-06 15:23:19 +0500

    ahhhhhhhhhh

--- Backend Files/schoolmanagementsystem.py
+++ Backend Files/schoolmanagementsystem.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request, url_for, redirect
 from database import create_app
 from admin import Admin
-
+from student import Student
 app, db = create_app()
 
 class SchoolManagementSystem:
     
     __admin_ = None
+    __student_= None
     __name = None
     def __init__(self):
         self.name = "School Management System"
         self.admin_ = None
+        self.student_=None
 
     def adminLogin(self,user,password_): 
             
@@ -18,6 +20,17 @@ class SchoolManagementSystem:
                 print('creating admin object')
                 temp = Admin(username=user, password=password_)
                 boolean, admin_ = temp.Login()        #here the admin_ will be initialized and will perform the operations along this object
+                return boolean
+            except Exception as e:
+                print(f"Exception: {e}")
+                return False
+    
+    def StudentLogin(self,user,password_): 
+            
+            try:
+                print('creating Student object')
+                temp = Student(username=user, password=password_)
+                boolean, studnet_ = temp.Login()        #here the student_ will be initialized and will perform the operations along this object
                 return boolean
             except Exception as e:
                 print(f"Exception: {e}")
@@ -47,7 +60,8 @@ def callLogin():
        elif(user_type=='teacher'):
              pass
        else:
-              pass
+            if (sms.StudentLogin(username, password)==True):
+               return render_template("stddash.html")
        return LoadLoginPage("Incorrect Credentials!")
 
 
@@ -59,4 +73,5 @@ def callLogout():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
+
--- Backend Files/student.py
+++ Backend Files/student.py
@@ -1,36 +1,35 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
+from sqlalchemy import text, Column, Integer, String, PrimaryKeyConstraint
 from Users import Users
 from database import create_app
-from FeeChallan import FeeChallan
+#from FeeChallan import FeeChallan
 
 app,db = create_app()
 
 class Student(Users, db.Model):
-    __tablename__ = 'students'
+    _tablename_ = 'student'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    class_name = db.Column(db.String(50), nullable=False)
-    parent_id = db.Column(db.Integer, db.ForeignKey('parents.id'), nullable=False)
-    phone_number = db.Column(db.String(15), nullable=False)
-    address = db.Column(db.String(255), nullable=False)
-    gender = db.Column(db.String(10), nullable=False)
-    fee_concession = db.Column(db.Float, default=0.0)
-    
-    # Directly include a reference to the FeeChallan
-    fee_challan = db.relationship('FeeChallan', backref='student', uselist=False, lazy=True)
+    sno = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(255))
+    username = db.Column(db.String(50), primary_key=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=True)
+    contact_number = db.Column(db.String(15), nullable=True)
+    enrolled_class = db.Column(db.Integer, nullable=False)
+    Gender = db.Column(db.String(255),nullable=False)
+    parent_ID = db.Column(db.String(15),nullable=False)
+    address = db.Column(db.String(255),nullable=False)
+    parent_contact=db.Column(db.String(50), nullable= False)
 
-    def __init__(self, name, class_name, parent, phone_number, address, gender, fee_concession=0.0):
-        self.name = name
-        self.class_name = class_name
-        self.parent = parent
-        self.phone_number = phone_number
-        self.address = address
-        self.gender = gender
-        self.fee_concession = fee_concession
-
+    def __init__(self, name=None, username=None, password=None, email=None, contact_number=None, enrolled_class = None, Gender = None, parent_ID = None, address=None, parent_contact=None):
+        super().__init__(name, username, password, email, contact_number)
+        self.enrolled_class=enrolled_class
+        self.Gender=Gender
+        self.parent_ID=parent_ID
+        self.address=address
+        self.parent_contact=parent_contact
+ 
     def setName(self, name):
         self.name = name
 
@@ -49,8 +48,8 @@ class Student(Users, db.Model):
     def setGender(self, gender):
         self.gender = gender
 
-    def setFeeConcession(self, fee_concession):
-        self.fee_concession = fee_concession
+    #def setFeeConcession(self, fee_concession):
+     #   self.fee_concession = fee_concession
 
     def getName(self):
         return self.name
@@ -73,26 +72,26 @@ class Student(Users, db.Model):
     def getFeeConcession(self):
         return self.fee_concession
 
-    def generateFeeChallan(self, month, fee_amount):
-        # Assuming FeeChallan class has a method to generate a fee challan
-        if not self.fee_challan:
-            challan = FeeChallan(student=self, month=month, fee=fee_amount)
-            db.session.add(challan)
-            db.session.commit()
+    # def generateFeeChallan(self, month, fee_amount):
+    #     # Assuming FeeChallan class has a method to generate a fee challan
+    #     if not self.fee_challan:
+    #         challan = FeeChallan(student=self, month=month, fee=fee_amount)
+    #         db.session.add(challan)
+    #         db.session.commit()
 
-    def getDetails(self):
-        details = f"Student Details:\nName: {self.name}\nClass: {self.class_name}\nParent: {self.parent.getName()}\nPhone Number: {self.phone_number}\nAddress: {self.address}\nGender: {self.gender}\nFee Concession: {self.fee_concession}"
+    # def getDetails(self):
+    #     details = f"Student Details:\nName: {self.name}\nClass: {self.class_name}\nParent: {self.parent.getName()}\nPhone Number: {self.phone_number}\nAddress: {self.address}\nGender: {self.gender}\nFee Concession: {self.fee_concession}"
         
-        if self.fee_challan:
-            details += f"\n\nFee Challan Details:\n{self.fee_challan.viewChallanDetails()}"
-        else:
-            details += "\nNo fee challan generated."
+    #     if self.fee_challan:
+    #         details += f"\n\nFee Challan Details:\n{self.fee_challan.viewChallanDetails()}"
+    #     else:
+    #         details += "\nNo fee challan generated."
 
-        return details
+    #     return details
 
-    def viewChallanDetails(self):
-        # Assuming FeeChallan class has a method to view details
-        if self.fee_challan:
-            return self.fee_challan.viewChallanDetails()
-        else:
-            return "No fee challan generated."
+    # def viewChallanDetails(self):
+    #     # Assuming FeeChallan class has a method to view details
+    #     if self.fee_challan:
+    #         return self.fee_challan.viewChallanDetails()
+    #     else:
+    #         return "No fee challan generated."
